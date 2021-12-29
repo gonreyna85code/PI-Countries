253f82a8dd0635456eaba07a36d2e2e4b201e4ad
gonzalo <gonreyna85@gmail.com>
2021-12-29 18:32:03 -0300

    x

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const DB_USER = 'postgres'
+const DB_PASSWORD = 'postgres'
+const DB_HOST = 'localhost'
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
   logging: false, // set to console.log to see the raw SQL queries
